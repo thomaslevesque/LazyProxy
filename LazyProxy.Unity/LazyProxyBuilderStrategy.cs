cc39ec1a10310040a02cb907212ae8ae6de012f4
Thomas Levesque <thomaslevesque@users.noreply.github.com>
2015-08-30 05:03:59 +0200

    Use more idiomatic Unity technique
    Use NewBuildPlan rather than injecting the container in the strategy

---
+++ LazyProxy.Unity/LazyProxyBuilderStrategy.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Practices.ObjectBuilder2;
+
+namespace LazyProxy.Unity
+{
+    class LazyProxyBuilderStrategy : BuilderStrategy
+    {
+        public override void PreBuildUp(IBuilderContext context)
+        {
+            if (context.Existing == null)
+            {
+                var policy = context.Policies.Get<ILazyProxyPolicy>(context.OriginalBuildKey);
+                if (policy != null)
+                {
+                    var lazyProxyType = LazyProxyGenerator.GetLazyProxyType(policy.ServiceType, policy.ImplementationType);
+                    context.Existing = context.NewBuildUp(new NamedTypeBuildKey(lazyProxyType, context.OriginalBuildKey.Name));
+                }
+            }
+            base.PreBuildUp(context);
+        }
+    }
+}
--- LazyProxy.Unity/LazyProxyExtension.cs
+++ LazyProxy.Unity/LazyProxyExtension.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using Microsoft.Practices.ObjectBuilder2;
-using Microsoft.Practices.Unity;
+﻿using Microsoft.Practices.Unity;
 using Microsoft.Practices.Unity.ObjectBuilder;
 
 namespace LazyProxy.Unity
@@ -9,33 +7,7 @@ namespace LazyProxy.Unity
     {
         protected override void Initialize()
         {
-            Context.Strategies.Add(new LazyProxyBuilderStrategy(Container), UnityBuildStage.PreCreation);
-        }
-
-        private class LazyProxyBuilderStrategy : BuilderStrategy
-        {
-            private readonly IUnityContainer _container;
-
-            public LazyProxyBuilderStrategy(IUnityContainer container)
-            {
-                _container = container;
-            }
-
-            public override void PreBuildUp(IBuilderContext context)
-            {
-                if (context.Existing == null)
-                {
-                    var policy = context.Policies.Get<ILazyProxyPolicy>(context.OriginalBuildKey);
-                    if (policy != null)
-                    {
-                        var lazyType = typeof(Lazy<>).MakeGenericType(policy.ImplementationType);
-                        var lazy = _container.Resolve(lazyType);
-                        var proxy = LazyProxyGenerator.CreateProxy(policy.ServiceType, policy.ImplementationType, lazy);
-                        context.Existing = proxy;
-                    }
-                }
-                base.PreBuildUp(context);
-            }
+            Context.Strategies.AddNew<LazyProxyBuilderStrategy>(UnityBuildStage.PreCreation);
         }
     }
 }
